3fe9446036a1af187e519360fd3638442810d184
unknown <ashuisalive@gmail.com>
2021-09-02 12:11:09 +0530

    updated bootstrap css

--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -1,7 +1,5 @@
 import React, {useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Button } from 'react-bootstrap';
-import Form from "react-bootstrap/Form";
 import { v4 as uuid } from 'uuid';
 import FormCard from './FormCard';
 
@@ -41,48 +39,49 @@ const SimpleForm = () => {
         setData([...data, payload]);
         console.log(data);
     }
-    const { name, email, comment, agreement, gender } = state;
+    const { name, email, comment, agreement } = state;
     return (
         <>
-            <Container className="w-50 border border-dark p-2 m-1 mx-auto rounded">
-                <h1 className="text-danger text-center">Bootstrap Forms</h1>
-                <Form>
-                    <Form.Group controlId="form.Name">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control ref={inputRef} name="name" value={name} onChange={handleChange} placeholder="Enter name" type="text" />
-                    </Form.Group>
-                    <Form.Group controlId="form.Email">
-                        <Form.Label>Email</Form.Label>
-                            <Form.Control name="email" value={email} onChange={handleChange} placeholder="name@example.com" type="email" />
-                            <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-                    </Form.Group>
-                    <Form.Group controlId="form.Textarea">
-                        <Form.Label>Comment</Form.Label>
-                        <Form.Control placeholder="Write your comments here..." name="comment" value={comment} onChange={handleChange} as="textarea" rows={3} />
-                    </Form.Group>
-                    <Form.Group className="m-2" controlId="formSelect">
-                        <Form.Control as="select" name="gender" onChange={handleChange}>
+            <div className="container w-90 p-2 m-1 mx-auto">
+                {/* <h1 className="text-danger text-center">Bootstrap Forms</h1> */}
+                <form className="form">
+                    <div className="form-group">
+                        <label className="form-label">Name</label>
+                        <input className="form-control" ref={inputRef} name="name" value={name} onChange={handleChange} placeholder="Enter name" type="text" />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label">Email</label>
+                        <input className="form-control" name="email" value={email} onChange={handleChange} placeholder="name@example.com" type="email" />
+                        <div className="form-text text-muted">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label">Comment</label>
+                        <textarea className="form-control" placeholder="Write your comments here..." name="comment" value={comment} onChange={handleChange} as="textarea" rows={3}></textarea>
+                    </div>
+                    <div className="form-group">
+                        <select className="form-select" as="select" name="gender" onChange={handleChange}>
                             <option>Select Gender</option>
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                             <option value="others">Others</option>
-                        </Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="form.File" className="m-3">
-                        <Form.Label>(Select Profile Picture)</Form.Label><br/>
-                        <Form.Control name="imageFile" ref={imageRef} type="file" />
-                    </Form.Group>
-                    <Form.Group controlId="form.Check">
-                            <Form.Check label="Click here to agree to terms and conditions." type="checkbox" name="agreement" checked={agreement} onChange={handleChange} />
-                    </Form.Group>
-                    <div className="text-center">
-                        <Button variant="primary" onClick={handleSubmit} type="submit">Submit</Button>
+                        </select>
                     </div>
-                </Form>
-            </Container>
-            <Container className="w-25 h-25 m-2">
+                    <div className="form-group m-3">
+                        <label className="form-label">(Select Profile Picture)</label><br/>
+                        <input className="form-control" name="imageFile" ref={imageRef} type="file" />
+                    </div>
+                    <div className="custom-control custom-checkbox">
+                        <input className="custom-control-input mx-1" type="checkbox" name="agreement" checked={agreement} onChange={handleChange} />
+                        <label className="custom-control-label">Click here to agree to terms and conditions.</label>
+                    </div>
+                    <div className="text-center">
+                        <button className="btn btn-primary" variant="primary" onClick={handleSubmit} type="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <div className="container w-25 h-25 m-2">
                 {data?.map((item) => <FormCard key={item.id} {...item} />)}
-            </Container>
+            </div>
         </>
     )
 }
